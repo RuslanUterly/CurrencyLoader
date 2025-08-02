5e69f867fe73e357fce179d139e61095722268a6
Ruslan Kholikov <kholikov.rus@mail.ru>
2025-08-03 00:14:12 +0300

    Refactoring db logic and di

---
+++ CurrencyLoader/Infrastucture/DbInitializer.cs
@@ -0,0 +1,39 @@
+using Npgsql;
+
+namespace CurrencyLoader.Infrastucture;
+
+public class DbInitializer
+{
+    private readonly NpgsqlDataSource _dataSource;
+    
+    public DbInitializer(NpgsqlDataSource dataSource)
+    {
+        _dataSource = dataSource;
+    }
+    
+    public async Task InitializeDatabase()
+    {
+        string createTablesSql = @"
+            CREATE TABLE IF NOT EXISTS currencies (
+                id SERIAL PRIMARY KEY,
+                code VARCHAR(10) UNIQUE NOT NULL,
+                name VARCHAR(100) NOT NULL
+            );
+
+            CREATE TABLE IF NOT EXISTS exchange_rates (
+                id SERIAL PRIMARY KEY,
+                currency_id INTEGER NOT NULL REFERENCES currencies(id),
+                date DATE NOT NULL,
+                value NUMERIC(10,4) NOT NULL
+            );
+
+            CREATE UNIQUE INDEX IF NOT EXISTS
+                idx_exchange_rates_currency_date
+            ON exchange_rates(currency_id, date);
+        ";
+    
+        await using NpgsqlConnection connection = await _dataSource.OpenConnectionAsync();
+        await using NpgsqlCommand command = new(createTablesSql, connection);
+        await command.ExecuteNonQueryAsync();
+    }
+}
